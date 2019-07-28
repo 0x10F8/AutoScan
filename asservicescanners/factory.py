6a5ee4a5e5e9d02db6f27d6e9455d2510cbb52f2
0x10F8 <cgardi203@caledonian.ac.uk>
2019-07-28 15:39:39 +0100

    Added HTTPS

--- asservicescanners/factory.py
+++ asservicescanners/factory.py
@@ -6,6 +6,7 @@ from asservicescanners.vncscanner import VNCScanner
 from asservicescanners.imapscanner import IMAPScanner
 from asservicescanners.sshscanner import SSHScanner
 from asservicescanners.ftpscanner import FTPScanner
+from asservicescanners.httpsscanner import HTTPSScanner
 
 def build_service_factory(ip, output_dir):
     # Mapping of NMAP token to the relevant scanner
@@ -16,6 +17,7 @@ def build_service_factory(ip, output_dir):
         "80/tcp": HTTPScanner(ip, output_dir),
         "110/tcp": POP3Scanner(ip, output_dir),
         "143/tcp": IMAPScanner(ip, output_dir),
+        "443/tcp": HTTPSScanner(ip, output_dir),
         "445/tcp": SMBScanner(ip, output_dir),
         "5900/tcp": VNCScanner(ip, output_dir)
     }
